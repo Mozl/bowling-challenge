9766c668e5381185d6602d43fc6e2f01f44a56db
Louis Moselhi <louismoselhi@macbook-pro-5.mynet>
2018-04-21 18:20:23 +0100

    added tests for rollCount and total

--- jasmine-standalone-3.1.0/spec/BowlingSpec.js
+++ jasmine-standalone-3.1.0/spec/BowlingSpec.js
@@ -1,14 +1,30 @@
 describe('Bowling', function(){
-  var bowling_game;
+  var game;
 
   beforeEach(function() {
-      bowling_game = new Bowling();
-    });
+    game = new Bowling();
+  });
 
   describe ('Constructor', function() {
-      it ('constructs a Bowling object with a default firstRollScore of 0', function() {
-          expect (bowling_game.firstRollScore).toEqual(0);
-      });
+    it ('constructs a Bowling object with a default firstRollScore of 0', function() {
+      expect (game.firstRollScore).toEqual(0);
+    });
   });
+
+  describe ('Rolling', function(){
+    it ('adds increases rollCount by 1 when rolling', function(){
+      game.firstRoll();
+      expect (game.rollCount).toEqual(2);
+    });
+  });
+  
+  describe ('Total', function(){
+    it ('sums the scores of the first and second rolls', function(){
+      game.firstRoll(4);
+      game.secondRoll(3);
+      expect (game.total()).toBe(7);
+    });
+  });
+
 
 });
